f4336b0534da25b2a6839bfa11e81616de3018c0
cmr <dev@cmr.cx>
2012-11-18 00:38:04 +0100

    MOTD

--- core.sql
+++ core.sql
@@ -345,7 +345,8 @@ CREATE TABLE "session" (
         "additional_secret"     TEXT,
         "expiry"                TIMESTAMPTZ     NOT NULL DEFAULT now() + '24 hours',
         "member_id"             INT8            REFERENCES "member" ("id") ON DELETE SET NULL,
-        "lang"                  TEXT );
+        "lang"                  TEXT,
+        "motd_seen"             TEXT            NOT NULL DEFAULT '' );
 CREATE INDEX "session_expiry_idx" ON "session" ("expiry");
 
 COMMENT ON TABLE "session" IS 'Sessions, i.e. for a web-frontend or API layer';
@@ -354,6 +355,7 @@ COMMENT ON COLUMN "session"."ident"             IS 'Secret session identifier (i
 COMMENT ON COLUMN "session"."additional_secret" IS 'Additional field to store a secret, which can be used against CSRF attacks';
 COMMENT ON COLUMN "session"."member_id"         IS 'Reference to member, who is logged in';
 COMMENT ON COLUMN "session"."lang"              IS 'Language code of the selected language';
+COMMENT ON COLUMN "session"."motd_seen"         IS 'The last "message of the day" displayed in this session';
 
 
 CREATE TABLE "policy" (
